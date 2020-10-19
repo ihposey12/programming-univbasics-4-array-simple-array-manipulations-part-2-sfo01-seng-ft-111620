b57a3d10f7b6cae674c21e41a5000f2de891251d
static-tutorial-1700 <ihposey@crimson.ua.edu>
2020-10-19 01:18:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_concat(arr1, arr2)
   arr1.concat(arr2)
 end
 
-def using_insert(arr, element)
-  arr.insert(element)
+def using_insert(array, element)
+  array.insert(element)
 end
 
 def using_uniq(array)
@@ -13,4 +13,11 @@ end
 def using_flatten(array)
   array.flatten
 end
-  
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
